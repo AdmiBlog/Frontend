3a80c758fa5073a4c320c2d0eac0b523961ff503
钦烨 <admibrill@outlook.com>
2026-02-14 13:39:49 +0800

    Update metingbase.ts

--- src/utils/metingbase.ts
+++ src/utils/metingbase.ts
@@ -3,7 +3,10 @@ import { MusicInfo } from "@/interfaces/music";
 
 export async function getPlayListInfo(platform: string,id: string):Promise<MusicInfo[]>{
     try{
-        let result: MusicInfo[];
+        let result: MusicInfo[]=[{
+            type: "error",
+            url: ""
+        }];
         const url="https://api.i-meto.com/meting/api?server="+platform+"&type=playlist&id="+id+"&r=0.122";
         await fetch(url,{next: { revalidate: 7200, tags: ["music"] }}).then(async(res) => res.ok ? (await res.json()): {})
         .then((data)=>{result=data;});
