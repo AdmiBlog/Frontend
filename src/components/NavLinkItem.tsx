aa8557599186212afbfc77fe8c3029441e41e031
钦烨 <admibrill@outlook.com>
2026-02-15 11:01:13 +0800

    Update

--- src/components/NavLinkItem.tsx
+++ src/components/NavLinkItem.tsx
@@ -1,24 +1,9 @@
 "use client";
-import { Icon } from "@iconify/react";
-import { ReactElement } from "react";
 import { NavLink } from "interfaces/navlink";
 import { siteConfigs } from "@/config";
 import { LazyImageWithFalldown } from "./ImageWithFalldown";
 
 export function NavLinkItem({ link }: { link: NavLink }) {
-  let latencyIcon: ReactElement = <></>;
-  if (link.latency! > 0) {
-    if (link.latency! <= 1)
-      latencyIcon = <Icon icon="system-uicons:signal-full" />;
-    else if (link.latency! <= 2)
-      latencyIcon = <Icon icon="system-uicons:signal-medium" />;
-    else if (link.latency! <= 3)
-      latencyIcon = <Icon icon="system-uicons:signal-low" />;
-    else latencyIcon = <Icon icon="system-uicons:signal-none" />;
-  } else
-    latencyIcon = (
-      <Icon icon="system-uicons:close" className="nav-none-icon" />
-    );
   return (
     <a
       className="nav-item cf-navs-link"
@@ -48,17 +33,6 @@ export function NavLinkItem({ link }: { link: NavLink }) {
       </div>
       <span className="nav-name cf-navs-name">{link.name}</span>
       <span className="nav-desc">{link.description}</span>
-      <span
-        className="nav-status"
-        title={
-          link.latency! > 0
-            ? "加载: " + Math.round(link.latency! * 1000) + "ms"
-            : "不可达"
-        }
-      >
-        {latencyIcon}
-        {/* {link.latency!>0?`${link.latency}s`:""} */}
-      </span>
     </a>
   );
 }
